712a5bc4dde8596b4e6bc63fffe712337e24b2d4
Madhuri Jagdeo <madhuri.jagdeo21@gmail.com>
2020-08-24 15:53:38 +0530

    Validated Last name

--- validation.sh
+++ validation.sh
@@ -4,10 +4,19 @@ shopt -s extglob
 
 read -p "Enter first name: " firstName
 firstNamePattern="^([A-Z]{1})+[a-z]{2,}$"
-
 if [[ $firstName =~ $firstNamePattern ]]
 then
         echo "First name is Valid"
 else
         echo "First name is not Valid"
 fi
+
+read -p "Enter Last name: " lastName
+lastNamePattern="^([A-Z]{1})+[a-z]{2,}$"
+if [[ $lastName =~ $lastNamePattern ]]
+then
+        echo "Last name is Valid"
+else
+        echo "Last name is not Valid"
+fi
+
